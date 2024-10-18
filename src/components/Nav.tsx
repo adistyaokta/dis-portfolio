58b3ccde4554fbe3c15ead375cf0b20c1ef6f1e6
adistyaokta <adiztya77@gmail.com>
2024-10-18 18:30:52 +0700

    init project

---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <nav className='container w-full sticky top-0 flex justify-between items-center px-1 py-2'>
+        <div className='w-1/3 text-left font-raleway font-semibold text-xl'>/</div>
+        <div className='w-1/3 text-center font-jersey text-3xl font-semibold tracking-wide'>dis</div>
+        <div className='w-1/3 text-right font-raleway font-semibold text-xl'>MENU</div>
+    </nav>
+  )
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#1E1E1C',  
+				secondary: '#E1E1E1',  
+				accent: '#008080',     
+			},
+		},
+		fontFamily: {
+			'raleway': ['Raleway', 'sans-serif'],
+			'jersey': ['"Jersey 25"'],
+		}
+	},
+	plugins: [],
+}
