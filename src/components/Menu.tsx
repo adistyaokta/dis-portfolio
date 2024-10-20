87a3f54784c7e8be5fd3d03c1251ee447d725103
adistyaokta <adiztya77@gmail.com>
2024-10-21 03:08:22 +0700

    feat: toggle menu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { isMenuOpen } from '../stores/app.store';
+import { useStore } from '@nanostores/react';
+import { cn } from '../utils/utils';
+import { Nav } from './Nav';
+
+export const Menu = () => {
+  return (
+    <aside className={cn('sticky top-0 flex flex-col overflow-hidden')}>
+      <div className=' flex flex-col py-4'>
+        <div className='text-7xl md:text-9xl h-20 md:h-full max-w-full w-full flex items-center justify-end uppercase font-extrabold hover:tracking-wider transition-all duration-300 ease-in-out'>
+          home
+        </div>
+        <div className='text-7xl md:text-9xl h-20 md:h-full max-w-full w-full flex items-center justify-end uppercase font-extrabold hover:tracking-wider transition-all duration-300 ease-in-out'>
+          project
+        </div>
+        <div className='text-7xl md:text-9xl h-20 md:h-full max-w-full w-full flex items-center justify-end uppercase font-extrabold hover:tracking-wider transition-all duration-300 ease-in-out'>
+          about
+        </div>
+        <div className='text-7xl md:text-9xl h-20 md:h-full max-w-full w-full flex items-center justify-end uppercase font-extrabold hover:tracking-wider transition-all duration-300 ease-in-out'>
+          contact
+        </div>
+      </div>
+      <div className='flex flex-col gap-2'>
+        <div className='text-lg w-full bg-secondary/80 text-primary'>
+          Find me on
+        </div>
+        <div className='flex flex-wrap justify-center items-center mt-auto'>
+          <div className='w-1/2 min-h-10 md:min-h-16 flex items-center text-left md:text-4xl border-t cursor-pointer'>
+            Email
+          </div>
+          <div className='w-1/2 min-h-10 md:min-h-16 flex items-center text-left md:text-4xl border-t cursor-pointer'>
+            Linkedin
+          </div>
+          <div className='w-1/2 min-h-10 md:min-h-16 flex items-center text-left md:text-4xl border-t cursor-pointer'>
+            Github
+          </div>
+          <div className='w-1/2 min-h-10 md:min-h-16 flex items-center text-left md:text-4xl border-t cursor-pointer'>
+            Instagram
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
+};
---
+++ src/components/Nav.tsx
@@ -0,0 +1,39 @@
+import { useStore } from '@nanostores/react';
+import { useEffect, useState } from 'react';
+import { isMenuOpen } from '../stores/app.store';
+import { Menu } from './Menu';
+
+export const Nav = () => {
+  const $isMenuOpen = useStore(isMenuOpen);
+  const toggleMenu = () => {
+    isMenuOpen.set(!$isMenuOpen);
+  };
+
+  useEffect(() => {
+    if ($isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [$isMenuOpen]);
+
+  return (
+    <div className='flex flex-col'>
+      <nav className='w-full min-w-full flex justify-between items-center px-1 py-2'>
+        <div className='w-1/3 h-10 max-h-10 flex items-center justify-start font-raleway font-semibold text-xl md:text-5xl'>
+          /
+        </div>
+        <div className='w-1/3 h-10 max-h-10 flex items-center justify-center font-jersey text-3xl md:text-7xl font-semibold tracking-wide'>
+          dis
+        </div>
+        <div
+          onClick={toggleMenu}
+          className='w-1/3 h-10 max-h-10 flex items-center justify-end font-raleway font-semibold text-xl md:text-3xl uppercase cursor-pointer select-none'
+        >
+          {$isMenuOpen ? 'Close' : 'Menu'}
+        </div>
+      </nav>
+      {$isMenuOpen && <Menu />}
+    </div>
+  );
+};
